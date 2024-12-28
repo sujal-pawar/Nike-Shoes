9a2469d6e032ac98158e38e4d6775ea725b1ad5f
sujal-pawar <sujalpawar00007@gmail.com>
2024-12-29 03:30:07 +0530

    update footer link

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -9,7 +9,7 @@ const footer = () => {
     <footer className='max-container'>
       <div className='flex justify-between items-start gap-20 flex-wrap max-lg:flex-col'>
         <div className='flex flex-col items-start'>
-          <a href="/">
+          <a href="#home">
             <img src={footerLogo} alt="footerlogo" 
             width={150}
             height={46}/>

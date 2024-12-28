7f0b146657633233b1d0df57d1e02612b4bbfe88
sujal-pawar <sujalpawar00007@gmail.com>
2024-12-29 00:32:13 +0530

    added Hero section and Popular Products section

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,47 +6,56 @@ import { hamburger } from '../assets/icons'
 const Nav = () => {
     const [isSidebarOpen, setIsSidebarOpen] = React.useState(false);
 
-    function toggleSidebar () {
+    function toggleSidebar() {
         setIsSidebarOpen(!isSidebarOpen);
+        document.getElementById('overlay').classList.remove('hidden');
     };
 
     return (
         <>
-            <header className='padding-x py-4 absolute z-10 w-full shadow-lg bg-white'>
+            <header className='padding-x py-4 absolute z-10 w-full bg-transparent'>
                 <nav className='flex justify-between items-center max-container'>
-                    <a href="/">
-                        <img src={headerLogo} alt="Logo" width={130} height={29} />
+                    <a href="/" className='left-0'>
+                        <img src={headerLogo} alt="Logo" width={70} height={29} className="m-2" />
                     </a>
 
                     <ul className='flex-1 flex justify-center items-center gap-16 max-md:hidden'>
                         {navLinks.map((item) => (
                             <li key={item.label}>
-                                <a href={item.href} className='font-montserrat leading-normal text-lg text-slate-gray'>
+                                <a href={item.href} className='font-montserrat leading-normal text-lg text-slate-900'>
                                     {item.label}
                                 </a>
                             </li>
                         ))}
                     </ul>
                     <div className='hidden max-md:block' onClick={toggleSidebar}>
-                        <img src={hamburger} alt="hamburger" width={25} height={25}/>
+                        <img src={hamburger} alt="hamburger" width={25} height={25} />
                     </div>
                 </nav>
             </header>
             {isSidebarOpen && (
-                <aside className='fixed top-0 right-0 w-60 max-md:w-43 h-full bg-slate-300 shadow-lg z-10'>
-                    <button onClick={toggleSidebar} className='absolute top-4 right-9'>
-                        <span className='text-2xl text-red-500'>X</span> 
-                    </button>
-                    <ul className='flex flex-col mt-10 justify-center items-center p-4'>
-                        {navLinks.map((item) => (
-                            <li key={item.label} className='m-5'>
-                                <a href={item.href} className='font-montserrat font-medium tracking-tighter hover:text-red-600  leading-normal text-lg text-slate-gray'>
-                                    {item.label}
-                                </a>
-                            </li>
-                        ))}
-                    </ul>
-                </aside>
+                <>
+                    <div
+                        id="overlay"
+                        className={`fixed inset-0 bg-black bg-opacity-20  ${isSidebarOpen ? 'block' : 'hidden'
+                            }`}
+                        onClick={toggleSidebar}
+                    ></div>
+                    <aside className='fixed top-0 right-0 w-60 max-md:w-43 h-full bg-white shadow-xl z-50'>
+                        <button onClick={toggleSidebar} className='absolute top-4 right-10'>
+                            <span className='text-2xl text-black hover:text-slate-gray'>X</span>
+                        </button>
+                        <ul className='flex flex-col mt-10 justify-start p-4'>
+                            {navLinks.map((item) => (
+                                <li key={item.label} className='m-5'>
+                                    <a href={item.href} className='asidebar'>
+                                        {item.label}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                    </aside>
+                </>
             )}
         </>
     )
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,10 +1,61 @@
 import React from 'react'
+import Button from '../components/Button';
+import { arrowRight } from '../assets/icons';
+import { shoes, statistics } from '../constants';
+import { bigShoe1 } from '../assets/images';
+import ShoeCard from '../components/ShoeCard';
+import { useState } from 'react';
 
 const Hero = () => {
+
+  const [bigShoeImg, setBigShoeImg] = useState(bigShoe1);
+
+  const changeBigShoeImage = (imgURL) => {
+    setBigShoeImg(imgURL)
+  }
+
   return (
-    <div>
-      Hero
-    </div>
+    <section
+    id='home'
+    className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container'>
+      <div className='relative xl:w-2/5 flex flex-col max-xl:padding-x pt-28'>
+      <h1 className='font-palanquin mt-10 text-8xl max-sm:text-[72px] font-bold max-sm:leading-[82px]'>
+      <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>The new Arrival</span><br />
+      <span className='text-coral-red inline-block mt-3'>Nike</span> Shoes
+      </h1>
+      <p className='font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm'>Dicover Stylish Nike arrivals, quality comfort, and  innovation for your active life </p>
+      <Button label="Shop now"
+      iconURL={arrowRight}
+      />
+
+      <div className='flex justify-start items-start flex-wrap w-full mt-6 gap-16'>
+      {statistics.map((stat,index)=>(
+        <div key={index} className='flex flex-col items-center gap-2'>
+          <p className='text-4xl max-sm:text-3xl font-palanquin font-bold'>{stat.value}</p>
+          <p className='leading-7 font-montserrat text-slate-gray'>{stat.label}</p>
+        </div>
+
+      ))}
+      </div>
+      </div>
+
+      <div className='relative flex-1 flex xl:min-h-screen max-xl:py-40 bg-hero bg-cover  bg-primary justify-center items-center'>
+        
+        <img src={bigShoeImg} alt="Shoe collection" width={610} height={500}
+        className='object-contain z-10 relative p-5' />
+
+        <div className='flex sm:gap-6 gap-4 absolute -bottom-[7%] max-sm:px-6'>
+          {shoes.map((shoe)=>(
+            <div>
+              <ShoeCard
+              imgURL={shoe} changeBigShoeImage={(shoe)=> changeBigShoeImage(shoe)} bigShoeImg={bigShoeImg}
+              />
+            </div>
+          ))}
+        </div>
+
+      </div>
+    </section>
   )
 }
 
